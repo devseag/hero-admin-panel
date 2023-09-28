1167b0efaed27cb5463f11596957f92ea2e0845a
devseag <noexample@gmail.com>
2023-09-27 22:19:59 -0300

    add redux thunk
    9 203

--- src/actions/actions/index_prior_to_9_203.js
+++ src/actions/actions/index_prior_to_9_203.js
@@ -1,17 +1,3 @@
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
-
-export const fetchFilters = (request) => (dispatch) => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-        .then(data => dispatch(filtersFetched(data)))
-        .catch(() => dispatch(filtersFetchingError()))
-}
-
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -56,15 +42,6 @@ export const activeFilterChanged = (filter) => {
         payload: filter
     }
 }
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000) 
-// }
 
 export const heroCreated = (hero) => {
     return {
